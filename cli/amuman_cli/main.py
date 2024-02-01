f3fd5bb8086251b6d86f42b5c6d102f3346a39a8
Mathieu Moalic <matmoa@pm.me>
2024-02-01 14:33:44 +0100

    Add better argument type hints

--- cli/amuman_cli/main.py
+++ cli/amuman_cli/main.py
@@ -1,5 +1,7 @@
 from pathlib import Path
 from typing import List
+from enum import Enum
+
 
 import httpx
 import toml
@@ -13,6 +15,18 @@ app = typer.Typer(rich_markup_mode="rich")
 default_config_path = xdg_config_home().joinpath("amuman/amuman.toml")
 
 
+class Priority(str, Enum):
+    Fast = "Fast"
+    Normal = "Normal"
+    Slow = "Slow"
+
+
+class GPUPartition(str, Enum):
+    Fast = "Fast"
+    Normal = "Normal"
+    Slow = "Slow"
+
+
 def init_config(config_path):
     print(
         f"[bold red]No config was found[/bold red] at `{config_path}`,[bold green] creating one:[/bold green]"
@@ -22,14 +36,32 @@ def init_config(config_path):
         "[bold green]AMUman manager URL [/bold green]",
         default="http://manager:8000",
     )
+    shared_dir_path = Prompt.ask(
+        "[bold green]Full path to the shared storage. [/bold green]",
+        default="/shared",
+    )
     with open(config_path, "w") as config_file:
-        data = {"manager_url": manager_url}
-        config_file.write(toml.dumps(data))
+        data = {
+            "manager_url": manager_url,
+            "shared_dir_path": shared_dir_path,
+        }
+        config = toml.dumps(data)
+        config_file.write(config)
+    print("[bold green]Successfully created the config file.[/bold green]")
+    print(
+        "[bold blue]Run `amuman-cli --install-completion` to benefit from shell completion. [/bold blue]"
+    )
+    return config
 
 
 def read_config(config_path):
     with open(config_path, "r") as config_file:
-        return toml.load(config_file)
+        config = toml.load(config_file)
+    required_keys = ["manager_url", "shared_dir_path"]
+    missing_keys = [key for key in required_keys if key not in config]
+    if missing_keys:
+        print(f"Missing keys: {missing_keys}")
+    return config
 
 
 @app.command()
@@ -43,6 +75,7 @@ def queue(
             writable=False,
             readable=True,
             resolve_path=True,
+            help="Paths to .mx3 files.",
         ),
     ],
     config_path: Annotated[
@@ -50,30 +83,33 @@ def queue(
         typer.Option(
             "--config",
             "-c",
+            help="Paths to the amuman-cli config file.",
         ),
     ] = default_config_path,
     priority: Annotated[
-        str,
+        Priority,
         typer.Option(
             "--priority",
             "-p",
-            help="Fast, Normal or Slow",
+            help="Job priority in the queue.",
+            case_sensitive=False,
         ),
-    ] = "Normal",
+    ] = Priority.Normal.name,
     gpu_partition: Annotated[
-        str,
+        GPUPartition,
         typer.Option(
             "--gpu-partition",
             "-g",
-            help="Fast, Normal or Slow",
+            help="Speed of GPUs that will run your jobs.",
+            case_sensitive=False,
         ),
-    ] = "Normal",
+    ] = GPUPartition.Normal.name,
     estimated_time: Annotated[
         int,
         typer.Option(
             "--estimated-time",
             "-e",
-            help="Estimated time for one simulations in hours",
+            help="Estimated time for one simulations in hours.",
             min=0,
             max=300,
         ),
@@ -82,24 +118,22 @@ def queue(
     if config_path.exists():
         config = read_config(config_path)
     else:
-        init_config(config_path)
-        config = read_config(config_path)
+        config = init_config(config_path)
 
     url = f"{config['manager_url']}/manager/task/add_task/"
     for path in paths:
         data = {
             "path": str(path),
-            "priority": priority,
-            "gpu_partition": gpu_partition,
+            "priority": priority.name,
+            "gpu_partition": gpu_partition.name,
             "est": estimated_time,
         }
-        print(data)
         try:
             response = httpx.post(url, data=data)
-            typer.echo(f"Path: {path} - Response Status: {response.status_code}")
-            typer.echo(f"Response Body: {response.text}\n---")
+            print(f"Path: {path} - Response Status: {response.status_code}")
+            # print(f"Response Body: {response.text}\n---")
         except httpx.HTTPError as e:
-            typer.echo(f"An HTTP error occurred for path {path}: {e}")
+            print(f"An HTTP error occurred for path {path}: {e}")
 
 
 def entrypoint():
