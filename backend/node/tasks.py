e18af1ad959bae9d449c95e662756a5b4b6a31b2
Mathieu Moalic <matmoa@pm.me>
2024-02-02 22:06:32 +0100

    Remove the old node implementation

--- backend/node/tasks.py
+++
@@ -1,7 +0,0 @@
-# from celery import shared_task
-# from .management.commands.runnode import Command as RunNodeCommand
-
-# @shared_task(bind=True)
-# def run_node_at_startup(self):
-#     run_node_command = RunNodeCommand()
-#     run_node_command.handle()
