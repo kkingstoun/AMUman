81551789bea8bef8a8e56c1e6bc26bd8399011ae
Mathieu Moalic <matmoa@pm.me>
2024-02-20 11:16:34 +0100

    Add Refresh button to navbar

---
+++ frontend/src/dashboard/navbar/Refresh.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import { NavLi } from 'flowbite-svelte';
+	import { Button } from 'flowbite-svelte';
+
+	import { lastFetchTime } from '$stores/store';
+	import { fetchJobs } from '$api/jobs';
+	import moment from 'moment';
+</script>
+
+<NavLi class="cursor-pointer" on:click={fetchJobs}>
+	Last refresh: {moment($lastFetchTime).format('HH:mm:ss')}</NavLi
+>
