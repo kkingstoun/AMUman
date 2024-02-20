c53824b5af1d87232720c949bdbb44c5bdd8726b
Mathieu Moalic <matmoa@pm.me>
2024-02-20 11:16:41 +0100

    Add color mapping and capitalize status in Status.svelte

--- frontend/src/lib/Status.svelte
+++ frontend/src/lib/Status.svelte
@@ -1,27 +1,40 @@
 <script lang="ts">
 	import { JobStatusEnum } from '../api/Api';
+	import { Badge } from 'flowbite-svelte';
 
 	export let status: JobStatusEnum | undefined;
+	type ColorType =
+		| 'none'
+		| 'red'
+		| 'yellow'
+		| 'green'
+		| 'indigo'
+		| 'purple'
+		| 'pink'
+		| 'blue'
+		| 'dark'
+		| 'primary'
+		| undefined;
 
 	// Define a mapping from job statuses to Tailwind CSS color classes
-	const statusColorMap: Record<JobStatusEnum, string> = {
-		[JobStatusEnum.WAITING]: 'bg-blue-500',
-		[JobStatusEnum.PENDING]: 'bg-blue-500',
-		[JobStatusEnum.RUNNING]: 'bg-green-500',
-		[JobStatusEnum.INTERRUPTED]: 'bg-red-500',
-		[JobStatusEnum.FINISHED]: 'bg-green-700'
+	const statusColorMap: Record<JobStatusEnum, ColorType> = {
+		[JobStatusEnum.WAITING]: 'yellow',
+		[JobStatusEnum.PENDING]: 'yellow',
+		[JobStatusEnum.RUNNING]: 'green',
+		[JobStatusEnum.INTERRUPTED]: 'red',
+		[JobStatusEnum.FINISHED]: 'indigo'
 	};
+	function capitalize(str: string | undefined) {
+		if (!str) return '';
+		return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
+	}
 
-	// Function to get color based on status
-	function getColorByStatus(status: JobStatusEnum | undefined): string {
-		if (!status) return 'bg-gray-200'; // Default color if status is not found
+	function getColorByStatus(status: JobStatusEnum | undefined): ColorType {
+		if (!status) return 'primary'; // Default color if status is not found
 		return statusColorMap[status];
 	}
 
 	$: color = getColorByStatus(status);
 </script>
 
-<span class="relative inline-block px-3 py-1 font-semibold leading-tight">
-	<span aria-hidden="true" class={`absolute inset-0 ${color} opacity-50 rounded-full`} />
-	<span class="relative">{status}</span>
-</span>
+<Badge class="font-extrabold" large color={getColorByStatus(status)}>{capitalize(status)}</Badge>
