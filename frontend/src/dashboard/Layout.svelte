8b132e368fae2de886041db6f24fef65f237b20c
Mathieu Moalic <matmoa@pm.me>
2024-02-21 08:35:43 +0100

    Remove footer, add github top navbar

--- frontend/src/dashboard/Layout.svelte
+++ frontend/src/dashboard/Layout.svelte
@@ -1,56 +1,25 @@
 <script lang="ts">
 	import 'tailwindcss/tailwind.css';
 
-	import TopBar from './NavBar.svelte';
+	import NavBar from './NavBar.svelte';
 	import Login from '$lib/Login.svelte';
-	import Footer from './Footer.svelte';
 	import Sidebar from './Sidebar.svelte';
 	import { isAuthenticated } from '$stores/store';
 	import { SvelteToast } from '@zerodevx/svelte-toast';
+	import { Alert } from 'flowbite-svelte';
 </script>
 
 <SvelteToast />
 {#if $isAuthenticated}
-	<TopBar />
-	<div class="background overflow-hidden w-full lg:p-4">
-		<div class="content flex flex-col justify-between overflow-hidden relative lg:rounded-2xl">
-			<div class="flex items-start flex-1">
-				<Sidebar />
-				<div class="flex flex-col flex-1 pl-0 w-full lg:space-y-4 lg:w-[calc(100%-16rem)]">
-					<main class="main flex-1 overflow-y-auto pb-36 pt-4 px-2 md:pb-8 md:px-4 lg:px-6">
-						<slot />
-					</main>
-				</div>
-			</div>
-			<Footer />
+	<div class="flex flex-col h-screen bg-gray-900">
+		<NavBar />
+		<div class="flex flex-1 overflow-hidden">
+			<Sidebar />
+			<main class="flex-1 overflow-y-auto p-4 md:p-8">
+				<slot />
+			</main>
 		</div>
 	</div>
 {:else}
 	<Login />
 {/if}
-
-<style>
-	.background {
-		/* background-image: url('./mac.webp'); */
-		background-color: rgb(43, 43, 56);
-		background-size: cover;
-		background-position: center;
-	}
-	.content {
-		background-color: rgba(16 18 27 / 40%);
-		backdrop-filter: blur(24px);
-	}
-	.main {
-		color: #f9fafb;
-		background-color: rgba(16 18 27 / 40%);
-		overflow: auto;
-	}
-	.main::-webkit-scrollbar {
-		width: 6px;
-		border-radius: 10px;
-	}
-	.main::-webkit-scrollbar-thumb {
-		background: rgb(1 2 3 / 40%);
-		border-radius: 10px;
-	}
-</style>
--- frontend/src/dashboard/NavBar.svelte
+++ frontend/src/dashboard/NavBar.svelte
@@ -1,17 +1,19 @@
 <script lang="ts">
-	import { Navbar, NavBrand, NavLi, NavUl, NavHamburger } from 'flowbite-svelte';
+	import { Navbar, NavBrand, NavUl, NavHamburger } from 'flowbite-svelte';
 	import ColumnFilters from './navbar/ColumnFiters.svelte';
 	import Logout from './navbar/Logout.svelte';
 	import Refresh from './navbar/Refresh.svelte';
+	import Github from './navbar/Github.svelte';
 </script>
 
-<Navbar>
+<Navbar class="!bg-gray-800">
 	<NavBrand href="/">
 		<img src="/images/logo.png" class="me-3 h-6 sm:h-9" alt="Flowbite Logo" />
 		<span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Amuman</span>
 	</NavBrand>
 	<NavHamburger />
 	<NavUl>
+		<Github />
 		<Refresh />
 		<ColumnFilters />
 		<Logout />
