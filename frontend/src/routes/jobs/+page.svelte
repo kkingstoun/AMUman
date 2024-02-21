f10e6e6496c99cbb8b622057e25cd688decb12d4
Mathieu Moalic <matmoa@pm.me>
2024-02-21 15:14:27 +0100

    Remove Refresh component and add it to Jobs page

--- frontend/src/routes/jobs/+page.svelte
+++ frontend/src/routes/jobs/+page.svelte
@@ -1,16 +1,31 @@
 <script lang="ts">
 	import JobList from '$lib/JobList.svelte';
+	import { RefreshOutline } from 'flowbite-svelte-icons';
+	import { lastFetchTime } from '$stores/store';
+	import { fetchJobs } from '$api/jobs';
+	import moment from 'moment';
+	import { onMount, onDestroy } from 'svelte';
+
+	let intervalId: ReturnType<typeof setInterval>;
+	let timeSinceLastFetch = moment($lastFetchTime).fromNow();
+
+	function refreshTimeSinceLastFetch() {
+		timeSinceLastFetch = moment($lastFetchTime).fromNow();
+	}
+	onMount(() => {
+		intervalId = setInterval(refreshTimeSinceLastFetch, 1000 * 60);
+	});
+
+	onDestroy(() => {
+		clearInterval(intervalId);
+	});
 </script>
 
-<svelte:head>
-	<title>Amuman - Jobs</title>
-</svelte:head>
-
-<div>
-	<div class="mt-6 mb-3 text-gray-400">Active jobs</div>
-	<JobList />
-	<div class="mt-6 mb-3 text-gray-400">Queued jobs</div>
-	<JobList />
-	<div class="mt-6 mb-3 text-gray-400">Finished jobs</div>
-	<JobList />
+<div class="flex pb-4 items-center">
+	<h2 class="text-4xl">Jobs</h2>
+	<div class="ml-7 text-gray-500">
+		Last refresh:{moment($lastFetchTime).format('HH:mm:ss')} ({timeSinceLastFetch})
+	</div>
+	<RefreshOutline on:click={fetchJobs} class="ml-3 text-gray-500" />
 </div>
+<JobList />
