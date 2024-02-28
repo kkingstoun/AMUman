7fd2cace5404628abb9788e6bd14d2629f7e5188
Mathieu Moalic <matmoa@pm.me>
2024-02-28 11:03:30 +0100

    Add detailed view with syntax highlighting

---
+++ frontend/src/lib/Drawer/Output.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+	import type { Job } from '$api/Api';
+	export let job: Job;
+	import Prism from 'prismjs';
+	import 'prismjs/components/prism-go'; // Ensure the import path is correct
+
+	let output = '';
+	onMount(() => {
+		output = job.output || 'nope';
+	});
+</script>
+
+<strong class="inline-block w-32">output:</strong>
+<div class="text-sm whitespace-pre-wrap shadow p-3 bg-slate-900">
+	<div class="code">
+		{@html Prism.highlight(output, Prism.languages['go'], 'go')}
+	</div>
+</div>
+
+<svelte:head>
+	<link
+		href="https://cdnjs.cloudflare.com/ajax/libs/prism/1.22.0/themes/prism.min.css"
+		rel="stylesheet"
+	/>
+</svelte:head>
--- frontend/src/lib/Utils.ts
+++ frontend/src/lib/Utils.ts
@@ -1,5 +1,7 @@
 import type { ItemType } from '$stores/Tables';
+import { toasts, type ToastColor } from '$stores/Other';
 import moment from 'moment';
+import type { Job, Gpus, Nodes } from '$api/Api';
 
 export function formatString(input: string): string {
     const replacedString = input.replace(/_/g, ' ');
@@ -19,4 +21,56 @@ export function formatValue(
         return moment(value).fromNow();
     }
     return value;
+}
+
+export type ColorType =
+    | 'none'
+    | 'red'
+    | 'yellow'
+    | 'green'
+    | 'indigo'
+    | 'purple'
+    | 'pink'
+    | 'blue'
+    | 'dark'
+    | 'primary'
+    | undefined;
+
+export function newToast(message: string, color: ToastColor): void {
+    toasts.update((t) => {
+        const toast = { message, color };
+        return [...t, toast];
+    });
+    // // Remove the toast after 3 seconds
+    // setTimeout(() => {
+    //     toasts.update((t) => {
+    //         return t.slice(1);
+    //     });
+    // }, 3000);
+
+}
+
+export function formatDate(time: string | undefined): string {
+    if (!time) return '-';
+    return moment(time).fromNow();
+}
+
+export function isJob(item: ItemType): item is Job {
+    return 'path' in item;
+}
+export function isNode(item: ItemType): item is Nodes {
+    return 'number_of_gpus' in item;
+}
+export function isGpu(item: ItemType): item is Gpus {
+    return 'speed' in item;
+}
+export function formatDateTime(dateString?: string | null): string {
+    if (!dateString) return '-';
+    return (
+        moment(dateString).format('YYYY-MM-DD HH:mm:ss') + ' (' + moment(dateString).fromNow() + ')'
+    );
+};
+
+export function getPropertyValue(item: ItemType, property: string): string | undefined {
+    return (item as any)[property];
 }
