6fc42b8309f7ef6189c7216072247decd902d2d0
Mathieu Moalic <matmoa@pm.me>
2024-03-04 14:22:48 +0100

    Fix cli to work with the new API

--- cli/entrypoint.sh
+++ cli/entrypoint.sh
@@ -2,8 +2,8 @@
 
 pip install -e .
 mkdir -p ~/.config/amuman
-echo 'manager_url = "http://amuman-manager:8000"
-shared_dir_root = "/nas"' > ~/.config/amuman/amuman.toml
+echo 'manager_url = "http://amuman-manager-dev:8000"
+shared_dir_root = "/mnt/smb"' > ~/.config/amuman/amuman.toml
 amuman-cli --install-completion bash
 source ~/.bash_completions/amuman-cli.sh
 bash
