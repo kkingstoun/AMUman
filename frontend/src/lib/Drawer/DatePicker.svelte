d7c07631b11cf944b828a9ba8e2a5e312a6d6d76
Mathieu Moalic <matmoa@pm.me>
2024-03-18 10:01:20 +0100

    Add barebone datepicker

---
+++ frontend/src/lib/Drawer/DatePicker.svelte
@@ -0,0 +1,13 @@
+<script>
+	import { DateInput } from 'date-picker-svelte';
+	let date = new Date();
+</script>
+
+<DateInput bind:value={date} />
+
+<style>
+	:root {
+		--date-picker-background: #1b1e27;
+		--date-picker-foreground: #f7f7f7;
+	}
+</style>
