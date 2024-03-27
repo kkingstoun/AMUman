b4484cbf11efc55c17169a8c2f9d615bf46d8518
Mathieu Moalic <matmoa@pm.me>
2024-03-27 13:53:09 +0100

    Remove logo link in login page

--- frontend/src/routes/login/+page.svelte
+++ frontend/src/routes/login/+page.svelte
@@ -9,13 +9,11 @@
 	class="main flex flex-col items-center justify-start h-screen w-full bg-gray-800 px-2 md:px-4 lg:px-6 pt-40"
 >
 	<div class="flex justify-center">
-		<a href="/">
-			<img
-				alt="amuman logo"
-				src="/images/logo.png"
-				class="h-40 mx-auto object-cover rounded-full w-40"
-			/>
-		</a>
+		<img
+			alt="amuman logo"
+			src="/images/logo.png"
+			class="h-40 mx-auto object-cover rounded-full w-40"
+		/>
 	</div>
 
 	<div id="form" class="flex flex-col items-center justify-center mt-4 rounded-lg">
